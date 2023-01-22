d386d3a77c51906813f3570b7f971061767ecca0
Tom Warne <twarne@gmail.com>
2023-01-21 20:19:46 -0700

    feat(api): add test data

--- src/order-reports-csv.test.ts
+++
@@ -1,8 +0,0 @@
-import OrderReportsCSV from './order-reports-csv';
-
-it('Should list files', async () => {
-  const orderReports = new OrderReportsCSV('reports/');
-
-  const result = await orderReports.initFiles();
-  expect(result).toBe(true);
-});
